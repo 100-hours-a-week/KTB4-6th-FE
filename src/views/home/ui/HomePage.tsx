7592bf85a7df0778ccaf8f99d6f826324073520c
서예원 <seoyaewon0724@gmail.com>
2026-09-20 19:12:30 +0900

    feat: 홈 화면에 홈 API 데이터 연결

--- src/views/home/ui/HomePage.tsx
+++ src/views/home/ui/HomePage.tsx
@@ -1,27 +1,35 @@
 'use client';
 
 import { useState } from 'react';
+import { useHome } from '@/features/home';
 import { NavigationSidebar } from '@/widgets/navigation-sidebar';
 import { CreateMeetingButton } from './CreateMeetingButton';
 import { HomeHeader } from './HomeHeader';
 import { MeetingListSection } from './MeetingListSection';
 import { TeamSummaryCard } from './TeamSummaryCard';
-import { mockMeetings, mockTeamSummary } from '../model/mock';
+import { toHomeViewModel } from '../model/home-view-model';
 
 export const HomePage = () => {
   const [isSidebarOpen, setIsSidebarOpen] = useState(false);
+  const { data: home } = useHome({ isEnabled: true });
+
+  if (!home) {
+    return null;
+  }
+
+  const { team, meetings } = toHomeViewModel(home);
 
   return (
     <div className="flex min-h-[844px] flex-1 flex-col bg-cool-50">
       <HomeHeader onMenuClick={() => setIsSidebarOpen(true)} />
-      <TeamSummaryCard team={mockTeamSummary} />
+      <TeamSummaryCard team={team} />
       <CreateMeetingButton />
-      <MeetingListSection meetings={mockMeetings} />
+      <MeetingListSection meetings={meetings} />
 
       <NavigationSidebar
         isOpen={isSidebarOpen}
         onOpenChange={setIsSidebarOpen}
-        teamName={mockTeamSummary.name}
+        teamName={team.name}
       />
     </div>
   );
