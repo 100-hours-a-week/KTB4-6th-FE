8b9c4dfdc640aecb69b9e19ff70ea2877816f540
서예원 <seoyaewon0724@gmail.com>
2026-09-24 20:30:54 +0900

    fix: 실시간 전사 스크롤과 새 발화 자동 스크롤

--- src/views/current-meeting/ui/CurrentMeetingPage.tsx
+++ src/views/current-meeting/ui/CurrentMeetingPage.tsx
@@ -81,7 +81,7 @@ export const CurrentMeetingPage = ({
   }
 
   return (
-    <div className="relative flex h-dvh min-h-[844px] flex-1 flex-col bg-cool-50">
+    <div className="relative flex h-dvh min-h-[844px] flex-col bg-cool-50">
       {!isPreview && !isCompleted && (
         <MeetingSseConnection meetingId={String(meetingId)} teamId={teamId} />
       )}
